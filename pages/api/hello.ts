4b190cb7f3b9b72121dcd57d5016a3aa1f706c5b
Muzaki 联想 <abdullah.muzaki@aruna.id>
2022-11-03 11:36:46 +0700

    POC connect prisma to mongodb

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import {PrismaClient} from '@prisma/client'
+import type {NextApiRequest, NextApiResponse} from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+async function main(prisma: any) {
+  // Connect the client
+  await prisma.$connect()
+  const post = await prisma.post.create({
+    data: {
+      title: 'foo',
+      slug: 'foo',
+      body: 'foo',
+    },
+  })
+
+  console.log(post)
+
+  // ... you will write your Prisma Client queries here
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const prisma = new PrismaClient()
+  main(prisma)
+  console.log(11)
+
+  res.status(200).json({name: 'John Doe'})
 }
